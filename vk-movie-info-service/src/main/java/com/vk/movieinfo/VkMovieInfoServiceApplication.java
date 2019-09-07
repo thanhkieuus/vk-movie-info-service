d9842bbd483556a7eb98ad931f8ef51b2b4cf97b
thanhkieuus <voltakieu@yahoo.com>
2019-09-07 18:27:03 -0500

    First Commit

---
+++ vk-movie-info-service/src/main/java/com/vk/movieinfo/VkMovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vk.movieinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkMovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkMovieInfoServiceApplication.class, args);
+	}
+
+}
