c3ff7abca5d25cdc6d91a9cfffc84fa38d0fe062
thanhkieuus <voltakieu@yahoo.com>
2019-09-12 11:22:17 -0500

    Updated to work with discovery server

--- vk-movie-info-service/src/main/java/com/vk/movieinfo/VkMovieInfoServiceApplication.java
+++ vk-movie-info-service/src/main/java/com/vk/movieinfo/VkMovieInfoServiceApplication.java
@@ -2,8 +2,10 @@ package com.vk.movieinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VkMovieInfoServiceApplication {
 
 	public static void main(String[] args) {
